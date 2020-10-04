9bc82929a693b95ee4facc41eea290643a4b59fd
pi <cjmugs@gmail.com>
2020-10-04 18:22:19 -0500

    new files

---
+++ fswebcam.py
@@ -0,0 +1,15 @@
+device / dev/video0
+input = 0
+loop = 15
+skip = 20
+background
+resolution = 1280 x 720
+set brightness = 60%
+set contrast = 13%
+top-banner
+font / usr/share/fonts/truetype/msttcorefonts/arial.ttf
+title "Basement Plant"
+timestamp "%d-%m-%Y %H:%M:%S (%Z)"
+jpeg = 95
+save / home/pi/Stream/Plant.jpg
+palette MJPEG
